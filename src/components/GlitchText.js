c7015288407ac78682f7ae69ef37e78befb33e01
huhuanming <workboring@gmail.com>
2018-08-25 17:31:52 +0800

    fix: fix glitch padding

--- src/components/GlitchText.js
+++ src/components/GlitchText.js
@@ -3,8 +3,10 @@ import './glitch.css'
 
 const GlitchText = ({ wrapperStyles = {}, textStyles = {} }) => {
   return (
-    <div className="glitch-effect" data-text="Hu Huanming">
-      Hu <span>Huanming</span>
+    <div style={{ paddingLeft: 10, paddingRight: 10 }}>
+      <div className="glitch-effect" data-text="Hu Huanming">
+        <span>Hu Huanming</span>
+      </div>
     </div>
   )
 }
